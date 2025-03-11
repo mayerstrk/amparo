c225203ecc4fcacac41074769d92ca72beded388
mstarkcode <mstarkcode@gmail.com>
2025-03-11 08:08:00 +0200

    remove import of requests .d.ts fille from commone/index.ts since it wasnt on the package

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,5 +2,4 @@ export * from "./base/builders/common.base.builders.controller";
 export * from "./classes/common.classes.errors";
 export * from "./enums/common.enums.error-names";
 export * from "./helpers/helpers.get-error-constructor";
-export * from "./types/common.types.requests";
 export * from "./utils/common.utils.assert-unreachable";
