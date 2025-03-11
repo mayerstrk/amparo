2c0f272a2522b0c8fb4ebe8a1538a3242c71d790
mstarkcode <mstarkcode@gmail.com>
2025-03-11 08:17:57 +0200

    fix errorname enum <- ErrorName

--- src/common/enums/common.enums.error-names.ts
+++ src/common/enums/common.enums.error-names.ts
@@ -1,14 +1,14 @@
 const enum ErrorName {
-    badRequest = "BadRequestError",
-    validation = "ValidationError",
-    notFound = "NotFoundError",
-    cast = "CastError",
-    duplicateKey = "DuplicateKeyError",
-    authentication = "AuthenticationError",
-    authorization = "AuthorizationError",
-    internalServerError = "InternalServerError",
-    forbidden = "ForbiddenError",
-    conflict = "ConflictError",
+  badrequest = "badrequesterror",
+  validation = "validationerror",
+  notfound = "notfounderror",
+  cast = "casterror",
+  duplicatekey = "duplicatekeyerror",
+  authentication = "authenticationerror",
+  authorization = "authorizationerror",
+  internalservererror = "internalservererror",
+  forbidden = "forbiddenerror",
+  conflict = "conflicterror",
 }
 
-export { ErrorName }
+export { ErrorName };
